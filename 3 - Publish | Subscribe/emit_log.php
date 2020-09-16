ac25b79aad3ef4bbd3565ca2e64141c6038c7fe7
Jesús Valera Reales <jesus1902@outlook.com>
2020-09-16 13:16:54 +0200

    Publish / Subscribe

---
+++ 3 - Publish | Subscribe/emit_log.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$host = $_ENV['RABBIT_HOST'];
+$port = $_ENV['RABBIT_PORT'];
+$username = $_ENV['RABBIT_USERNAME'];
+$password = $_ENV['RABBIT_PASSWORD'];
+
+$connection = new AMQPStreamConnection($host, $port, $username, $password);
+$channel = $connection->channel();
+
+$channel->exchange_declare('logs', 'fanout', false, false, false);
+
+$data = implode(' ', array_slice($argv, 1));
+if (empty($data)) {
+    $data = 'info: Hello World!';
+}
+$message = new AMQPMessage($data);
+
+$channel->basic_publish($message, 'logs');
+
+echo ' [x] Sent ', $data, "\n";
+
+$channel->close();
+$connection->close();
---
+++ 3 - Publish | Subscribe/receive_logs.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$host = $_ENV['RABBIT_HOST'];
+$port = $_ENV['RABBIT_PORT'];
+$username = $_ENV['RABBIT_USERNAME'];
+$password = $_ENV['RABBIT_PASSWORD'];
+
+$connection = new AMQPStreamConnection($host, $port, $username, $password);
+$channel = $connection->channel();
+
+$channel->exchange_declare('logs', 'fanout', false, false, false);
+
+[$queueName] = $channel->queue_declare('', false, false, true, false);
+$channel->queue_bind($queueName, 'logs');
+
+echo " [x] Waiting for logs. To exit press Ctrl+C\n";
+
+$callback = static function ($message) {
+    echo ' [x] Received ', $message->body, "\n";
+};
+
+$channel->basic_consume($queueName, '', false, true, false, false, $callback);
+
+while ($channel->is_consuming()) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
